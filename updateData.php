416f186dc2bacd6957c8c8aea47d9879cc272af6
Nikolaos Dimopoulos <nikos@niden.net>
2020-02-10 08:46:42 -0500

    Removing update for sponsors

--- updateData.php
+++ updateData.php
@@ -1,16 +1,16 @@
 <?php
 
-$base      = "/opt/build/repo/";
-echo "Updating Sponsors" . PHP_EOL;
-
-$data = file_get_contents(
-    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/sponsors-fragment.html'
-);
-
-file_put_contents(
-    $base . '_includes/sponsors.html',
-    $data
-);
+//$base      = "/opt/build/repo/";
+//echo "Updating Sponsors" . PHP_EOL;
+//
+//$data = file_get_contents(
+//    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/sponsors-fragment.html'
+//);
+//
+//file_put_contents(
+//    $base . '_includes/sponsors.html',
+//    $data
+//);
 
 echo "Updating Fan Art" . PHP_EOL;
 
