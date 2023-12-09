d2341841ee0d035fa0353fc855f7565c374f9908
Ihor Sychevskyi <arhell333@gmail.com>
2023-12-10 00:11:10 +0200

    fix sponsors block (#211)

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 BUILD_BASE="/opt/buildhome/repo"
 INPUT_BASE="https://raw.githubusercontent.com/phalcon/assets/master/phalcon"
 
-echo "Updating Sponsors"
-wget -O "$BUILD_BASE/_includes/sponsors.html" "$INPUT_BASE/sponsors-fragment.html"
-
 echo "Updating Fan Art"
 wget -O "$BUILD_BASE/_includes/fanart.html" "$INPUT_BASE/fanart-fragment.html"
 
