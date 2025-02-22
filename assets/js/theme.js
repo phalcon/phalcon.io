3c3a9f3fd589f627f88e74525228fea9e39c1c74
Ihor Sychevskyi <arhell333@gmail.com>
2025-02-23 00:05:52 +0200

    update theme (#303)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,59 +1,29 @@
-function updateDarkMode(darkMode) {
-    if (!darkMode && navigator.userAgent.indexOf('median') < 0) {
-        darkMode = (document.cookie.match(/^(?:.*;)?\s*darkMode\s*=\s*([^;]+)(?:.*)?$/) || [,"auto"])[1];
-    }
+class LightDark {
 
-    if (typeof median !== "undefined") {
-        median.screen.setColorScheme(darkMode);
-    } else {
-        document.documentElement.setAttribute("data-color-scheme-option", darkMode);
-    }
+    constructor() {
+        const html = document.documentElement, mode = localStorage.getItem('mode') || 'auto'
+        const getPreferredMode = () => matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 
-    if (darkMode !== "light" && darkMode !== "dark") {
-        darkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-    }
-    document.documentElement.setAttribute("color-scheme", darkMode);
-}
+        const switchMode = mode => {
+            const newMode = mode === 'auto' ? getPreferredMode() : mode
+            html.style.colorScheme = mode === 'auto' ? 'light dark' : newMode
 
-updateDarkMode();
+            html.classList.remove('light', 'dark')
+            html.classList.add(`${newMode}`)
+            localStorage.setItem('mode', mode)
 
-function median_library_ready() {
-    updateDarkMode();
-}
-
-if (window.matchMedia) {
-    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function () {
-        const darkMode = document.documentElement.getAttribute("data-color-scheme-option");
-        if (darkMode === "light") {
-            console.log("Light mode and detected mode switch.");
-            document.documentElement.setAttribute("color-scheme", "light");
-        } else if (darkMode === "dark") {
-            console.log("Dark mode and detected mode switch.");
-            document.documentElement.setAttribute("color-scheme", "dark");
-        } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            console.log("Auto mode and detected switch to Dark Mode.");
-            document.documentElement.setAttribute("color-scheme", "dark");
-        } else {
-            console.log("Auto mode and detected switch to Light Mode.");
-            document.documentElement.setAttribute("color-scheme", "light");
+            document.querySelectorAll('[data-mode]').forEach(el => el.classList.toggle('active', el.dataset.mode === mode))
         }
-    });
+
+        matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+            if (localStorage.getItem('mode') === 'auto') switchMode('auto')
+        })
+
+        document.addEventListener('click', e => e.target.dataset.mode && switchMode(e.target.dataset.mode))
+
+        switchMode(mode)
+    }
+
 }
 
-document.addEventListener("click", function (event) {
-        if (event.target.matches("#dark-mode-button")) {
-            event.preventDefault();
-            document.cookie = "darkMode=dark";
-            updateDarkMode("dark");
-        } else if (event.target.matches("#light-mode-button")) {
-            event.preventDefault();
-            document.cookie = "darkMode=light";
-            updateDarkMode("light");
-        } else if (event.target.matches("#auto-mode-button")) {
-            event.preventDefault();
-            document.cookie = "darkMode=auto";
-            updateDarkMode("auto");
-        }
-    },
-    false
-);
+new LightDark()
