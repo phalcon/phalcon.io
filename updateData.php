500d90433cd6bd29e6f93f2943c2a83b3e9eafe9
Nikolaos Dimopoulos <nikos@niden.net>
2025-03-19 22:38:34 +0000

    Update updateData.php

--- updateData.php
+++ updateData.php
@@ -1,16 +1,6 @@
 <?php
 
-$base      = "/opt/build/repo/";
-//echo "Updating Sponsors" . PHP_EOL;
-//
-//$data = file_get_contents(
-//    'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/sponsors-fragment.html'
-//);
-//
-//file_put_contents(
-//    $base . '_includes/sponsors.html',
-//    $data
-//);
+$base      = dirname(__FILE__) . '/';
 
 echo "Updating Fan Art" . PHP_EOL;
 
